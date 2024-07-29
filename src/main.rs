b704548b326b8b0190aa7fd94e4faf1eaa3d5780
kktsdev <kpiatkowski@protonmail.com>
2024-07-29 20:23:57 +0200

    Added support for 256 colors

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,7 @@ use thiserror::Error;
 
 const BUILTIN_CONFIG: &str = include_str!("yaf.conf");
 static STYLES: phf::Map<&str, &str> = phf_map! {
-    "color0" => "\x1B[0;30m",
-    "color1" => "\x1B[0;31m",
-    "color2" => "\x1B[0;32m",
-    "color3" => "\x1B[0;33m",
-    "color4" => "\x1B[0;34m",
-    "color5" => "\x1B[0;35m",
-    "color6" => "\x1B[0;36m",
-    "color7" => "\x1B[0;37m",
-    "color8"  => "\x1B[1;30m",
-    "color9"  => "\x1B[1;31m",
-    "color10" => "\x1B[1;32m",
-    "color11" => "\x1B[1;33m",
-    "color12" => "\x1B[1;34m",
-    "color13" => "\x1B[1;35m",
-    "color14" => "\x1B[1;36m",
-    "color15" => "\x1B[1;37m",
+    "color" => "\x1B[38;5;{}m",
     "bold" => "\x1B[1m",
     "italic" => "\x1B[3m",
     "underline" => "\x1B[4m",
@@ -150,8 +135,8 @@ fn parse_line(line: &str) -> Result<String, MyError> {
                 if buffer.starts_with('$') {
                     output.push_str(&get_env(&buffer[1..])?);
                 } else if buffer.starts_with('@') {
-                    if let Some(replacement) = STYLES.get(&buffer.as_str()[1..]) {
-                        output.push_str(replacement);
+                    if let Some(replacement) = get_style(&buffer.as_str()[1..]) {
+                        output.push_str(&replacement);
                     } else {
                         return Err(MyError::UnknownBuiltinVariable(buffer.clone()));
                     }
@@ -175,6 +160,19 @@ fn parse_line(line: &str) -> Result<String, MyError> {
 
     output.push('\n');
     Ok(output)
+}
+
+fn get_style(key: &str) -> Option<String> {
+    if key.starts_with("color") {
+        let suffix = &key["color".len()..].trim();
+        if !suffix.is_empty() {
+            if let Some(format_string) = STYLES.get("color") {
+                return Some(format!("{}", format_string.replace("{}", suffix)));
+            }
+        }
+    }
+
+    STYLES.get(key).map(|&value| value.to_string())
 }
 
 fn run_sh(command: &str) -> Result<String, MyError> {
