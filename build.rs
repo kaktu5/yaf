6990182b9e5dad8696a83cac7a92d2a428aaf03a
kktsdev <108426150+kktsdev@users.noreply.github.com>
2024-10-16 18:09:31 +0200

    get commit hash without git

--- build.rs
+++ build.rs
@@ -1,10 +1,15 @@
-use std::process::Command;
+use std::{fs::read_to_string, path::Path};
 
 fn main() {
-    let output = Command::new("git")
-        .args(&["rev-parse", "--short", "HEAD"])
-        .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
-    println!("cargo:rustc-env=GIT_COMMIT_HASH={}", git_hash.trim());
+    println!("cargo:rustc-env=GIT_COMMIT_HASH={}", get_commit_hash());
+}
+
+fn get_commit_hash() -> String {
+    let git_head =
+        read_to_string(Path::new(".git/HEAD")).unwrap_or_else(|_| String::from("unknown"));
+    let ref_path = Path::new(".git").join(git_head.trim().split_whitespace().last().unwrap());
+    read_to_string(ref_path)
+        .unwrap_or_else(|_| String::from("unknown"))
+        .trim()[..7]
+        .to_string()
 }
