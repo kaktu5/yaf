321af869d201f05aa961ef7e56a1051890a99df6
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-01-12 19:43:50 +0100

    small changes

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() {
 fn get_commit_hash() -> String {
     let git_head =
         read_to_string(Path::new(".git/HEAD")).unwrap_or_else(|_| String::from("unknown"));
-    let ref_path = Path::new(".git").join(git_head.trim().split_whitespace().last().unwrap());
+    let ref_path = Path::new(".git").join(git_head.split_whitespace().last().unwrap());
     read_to_string(ref_path)
         .unwrap_or_else(|_| String::from("unknown"))
         .trim()[..7]
